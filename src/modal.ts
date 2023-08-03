cffe2defa173278adeec98f46d35f1be590ef9dc
kaynaatfatima <kaynaat.fatima@jbs.live>
2023-08-03 16:52:19 +0500

    Changed logic - TO: payment gateway website will open in an iframe and send postMessage of embedded. If embedded=true: then payment gateway website will keep opening in the iframe. if !embedded: url will be changed to the payment gateway website url.
    - FROM:  payment gateway website will be opened in new tab and send postMessage to websiteA.
    and then embedded iframe will be opened and we probably won't be able to close that... and it will stay open in that case.

--- src/modal.ts
+++ src/modal.ts
@@ -1,47 +1,43 @@
-// modal.ts
 export function openModal(apiKey: string) {
-  // Send the API key to paymentGatewayWeb
+  // Send the API key to paymentGatewayWebsite
   const paymentGatewayWebUrl = `http://127.0.0.1:5173/`;
   const paymentGatewayWebURL_key = `${paymentGatewayWebUrl}payment?key=${apiKey}`;
-  const paymentGatewayWebWindow = window.open(paymentGatewayWebURL_key, "_blank");
 
-  // Listen for messages from paymentGatewayWeb
+  // Code to display the paymentGatewayWebsite in an embedded modal on websiteA
+  const modalOverlay = document.createElement("div");
+  modalOverlay.classList.add("modal-overlay");
+
+  const closeButton = document.createElement("button");
+  closeButton.classList.add("modal-close-btn");
+  closeButton.innerText = "X";
+  closeButton.addEventListener("click", () => closeModal());
+  modalOverlay.appendChild(closeButton);
+
+  const iframe = document.createElement("iframe");
+  iframe.src = paymentGatewayWebURL_key + "&embedded=true";
+  iframe.setAttribute("allowfullscreen", "");
+  iframe.setAttribute("frameborder", "0");
+  modalOverlay.appendChild(iframe);
+
+  document.body.appendChild(modalOverlay);
+
+  const closeModal = (): void => {
+    document.body.removeChild(modalOverlay);
+  };
+
+  // Listen for messages from paymentGatewayWebsite
   window.addEventListener("message", receiveMessage, false);
 
   function receiveMessage(event: MessageEvent) {
-    // Check the origin of the message to ensure it's coming from paymentGatewayWebsite
+    // Ensure origin of the message
     if (event.origin === paymentGatewayWebUrl) {
       const data = event.data;
-
-      // Assuming the response data contains the 'embedded' property indicating whether it's embedded or not
       const embedded = data.embedded;
 
-      if (embedded) {
-        // Code to display the paymentGatewayWeb in an embedded modal on qrWebPage
-        const modalOverlay = document.createElement("div");
-        modalOverlay.classList.add("modal-overlay");
-
-        const closeButton = document.createElement("button");
-        closeButton.classList.add("modal-close-btn");
-        closeButton.innerText = "X";
-        closeButton.addEventListener("click", () => closeModal());
-        modalOverlay.appendChild(closeButton);
-
-        const iframe = document.createElement("iframe");
-        iframe.src = paymentGatewayWebURL_key + "&embedded=true";
-        iframe.setAttribute("allowfullscreen", "");
-        iframe.setAttribute("frameborder", "0");
-        modalOverlay.appendChild(iframe);
-
-        document.body.appendChild(modalOverlay);
-
-        const closeModal = (): void => {
-          document.body.removeChild(modalOverlay);
-        };
-      } else {
-        // embedded=false
-        // Open paymentGatewayWebsite in the same tab as qrWebPage
-        paymentGatewayWebWindow.location.href = paymentGatewayWebURL_key;
+      if (!embedded) {
+        // embedded=false, close the modal and open paymentGatewayWebsite in a new tab
+        closeModal();
+        window.location.href = paymentGatewayWebURL_key;
       }
     }
   }
